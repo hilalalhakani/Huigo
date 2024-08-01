867a0db05ceca1f51a3fda2ccc3687ca1accf562
Hilal Hakkani <hilal.hakani@360mea.com>
2024-08-01 19:45:43 +0300

    Initial Commit

---
+++ Huigo/HuigoApp.swift
@@ -0,0 +1,17 @@
+//
+//  HuigoApp.swift
+//  Huigo
+//
+//  Created by Hilal Hakkani on 01/08/2024.
+//
+
+import SwiftUI
+
+@main
+struct HuigoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
